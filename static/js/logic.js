da065f272d160707fc143ab363d615e8577558c5
Ramyata <ramyata.upmaka@gmail.com>
2021-02-05 21:17:21 -0500

    need to fix colors

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,21 +9,12 @@ var myMap = L.map("map", {
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
-    maxZoom: 18,
+    maxZoom: 100,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
     }).addTo(myMap);
 
-// Define a markerSize function that will give each earthquake a different size by magnitude
-function markerSize(magnitude) {
-  return magnitude / 40;
-};
-
-// Define a markerColor function that will give each earthquake a different color by depth
-function markerColor(depth){
-  return depth
-};
 
 // GeoJson, Used the following data "Past 7 days, all earthquakes"
 url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
@@ -31,9 +22,8 @@ url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojso
 // Within d3, get the necessary data and print out a circle for each earthquake
 d3.json(url,function(data){
   // Getting all the necessary data from json file
-  var x;
   var mag = []; var lng = []; var lat = []; var depth = []; var time = []; var place = [];
-  for (x = 0; x < data.features.length; x++){
+  for (var x = 0; x < data.features.length; x++){
     mag.push(data.features[x].properties.mag);
     lng.push(data.features[x].geometry.coordinates[0]);
     lat.push(data.features[x].geometry.coordinates[1]);
@@ -42,22 +32,80 @@ d3.json(url,function(data){
     time.push(data.features[x].properties.time);
     place.push(data.features[x].properties.place);
   };
+  console.log(depth);
   // Get the markerSize and markerColor
-  
+  // Define a markerSize function that will give each earthquake a different size by magnitude
+  function markerSize(magnitude) {
+    return magnitude / 0.00005;
+  };
+
+  // Define a markerColor function that will give each earthquake a different color by depth
+  function markerColor(depth){
+    if(depth>-10 && depth<=10){
+      depthColor = "#006600";
+    }
+    else if(depth>10 && depth<=30){
+      depthColor = "#336210";
+    }
+    else if(depth>30 && depth<=50){
+      depthColor = "#805b28";
+    }
+    else if(depth>50 && depth<=70){
+      depthColor = "#b25738";
+    }
+    else if(depth>70 && depth<=90){
+      depthColor =  "#d95344";
+    }
+    else{
+      depthColor = "#ff5050";
+    }
+    return depthColor
+  };
+
+  function getColor(d) {
+    return d > 90  ? '#ff5050' :
+           d > 70  ? '#FF6666' :
+           d > 50  ? '#b27566' :
+           d > 30  ? '#99826d' :
+           d > 10  ? '#59a17f' :
+           d > 0   ? '#b27566' :
+           d > -10 ? '#d9635b' :
+                     '#ff5050';
+  };
+
   // Store all of the data in an object called earthquake
 
   // Loop through the earthquake array and create one circle for each earthquake object
-  for (var i = 0; i < cities.length; i++) {
-    L.circle(cities[i].location, {
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: "purple",
+  for (var i = 0; i < data.features.length; i++) {
+    L.circle([lat[i],lng[i]], {
+      fillOpacity: 0.5,
+      color: 'white',
+      fillColor: getColor(depth[i]),
       // Setting our circle's radius equal to the output of our markerSize function
-      // This will make our marker's size proportionate to its population
-      radius: markerSize(cities[i].population)
-      }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
-  }
-  console.log(place);
+      // This will make our marker's size proportionate to its magnitude
+      radius: markerSize(mag[i])
+      }).bindPopup("<h1>" + place[i] + "</h1> <hr> <h3>").addTo(myMap);
+  };
+
+  var legend = L.control({position:"bottomright"});
+  legend.onAdd = function(map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        depth_levels = ["-10", "10", "30" ," 50" ,"70" ," 90"],
+        //colors = ["#006600","#336210","#805b28","#b25738","#d95344","#ff5050"]; 
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < depth_levels.length; i++) {
+      div.innerHTML +=
+      '<i style="background:' + getColor(depth_levels[i] + 1) + '"></i> ' +
+      depth_levels[i] + (depth_levels[i + 1] ? '&ndash;' + depth_levels[i + 1] + '<br>' : '+');
+    };
+
+    return div;
+};
+
+legend.addTo(myMap);
 });
 
 
@@ -68,42 +116,42 @@ d3.json(url,function(data){
 
 
 // Each city object contains the city's name, location and population
-var cities = [
-  {
-    name: "New York",
-    location: [40.7128, -74.0059],
-    population: 8550405
-  },
-  {
-    name: "Chicago",
-    location: [41.8781, -87.6298],
-    population: 2720546
-  },
-  {
-    name: "Houston",
-    location: [29.7604, -95.3698],
-    population: 2296224
-  },
-  {
-    name: "Los Angeles",
-    location: [34.0522, -118.2437],
-    population: 3971883
-  },
-  {
-    name: "Omaha",
-    location: [41.2524, -95.9980],
-    population: 446599
-  }
-];
+// var cities = [
+//   {
+//     name: "New York",
+//     location: [40.7128, -74.0059],
+//     population: 8550405
+//   },
+//   {
+//     name: "Chicago",
+//     location: [41.8781, -87.6298],
+//     population: 2720546
+//   },
+//   {
+//     name: "Houston",
+//     location: [29.7604, -95.3698],
+//     population: 2296224
+//   },
+//   {
+//     name: "Los Angeles",
+//     location: [34.0522, -118.2437],
+//     population: 3971883
+//   },
+//   {
+//     name: "Omaha",
+//     location: [41.2524, -95.9980],
+//     population: 446599
+//   }
+// ];
 
-// Loop through the cities array and create one marker for each city object
-for (var i = 0; i < cities.length; i++) {
-  L.circle(cities[i].location, {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: "purple",
-    // Setting our circle's radius equal to the output of our markerSize function
-    // This will make our marker's size proportionate to its population
-    radius: markerSize(cities[i].population)
-  }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
-}
+// // Loop through the cities array and create one marker for each city object
+// for (var i = 0; i < cities.length; i++) {
+//   L.circle(cities[i].location, {
+//     fillOpacity: 0.75,
+//     color: "white",
+//     fillColor: "purple",
+//     // Setting our circle's radius equal to the output of our markerSize function
+//     // This will make our marker's size proportionate to its population
+//     radius: markerSize(cities[i].population)
+//   }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+// }
